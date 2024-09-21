c3dbd7c5df519f92601b03533f792cc048b13c44
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2024-09-21 13:50:17 +0500

    commit

--- routes/addressRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitAddressForm, upload } = require('../controllers/addressController');
-
-// Route to handle form submission
-router.post('/submit', upload.single('w9File'), submitAddressForm);
-
-module.exports = router;
