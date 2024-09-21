f896dd5643117fc6cc2317ccad9ecbb9f43b2a4b
Abdul Rafay Ahmed <111084742+Rafay575@users.noreply.github.com>
2024-09-21 13:50:39 +0500

    commit

--- index.js
+++ index.js
@@ -2,28 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require('path');
 
 const authRoutes = require("./routes/authRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 const truckloadRoutes = require("./routes/truckloadRoutes");
-const addressRoutes = require('./routes/addressRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//middlewares using
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
+<<<<<<< HEAD
 // Serve uploaded files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+=======
+>>>>>>> parent of 37352a5 (commit address info api)
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api", truckloadRoutes);
-app.use('/api/address', addressRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
